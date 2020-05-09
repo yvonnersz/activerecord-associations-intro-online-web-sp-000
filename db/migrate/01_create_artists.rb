b5331e2050f6f962b2af6ed8da1e2288b207013c
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-09 12:38:03 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -2,8 +2,6 @@ class CreateArtists < ActiveRecord::Migration[4.2]
   def change
     create_table :artists do |t|
       t.string :name
-      t.integer :artist_id
-      t.integer :genre_id
     end
   end
 end
