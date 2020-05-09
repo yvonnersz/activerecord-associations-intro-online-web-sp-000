a7f8c86ace3070622a622e04a4d21e0ce1248a80
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-09 12:38:47 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,6 +1,6 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
   def change
-    create_table :artists do |t|
+    create_table :songs do |t|
       t.string :name
       t.integer :artist_id
       t.integer :genre_id
