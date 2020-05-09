b06ccab87487744ad9aff9558e1ee451035c48c5
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-09 12:37:06 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,8 +1,6 @@
 class CreateGenres < ActiveRecord::Migration[4.2]
   def change
-    def change
-      create_table :genres do |t|
-        t.string :name
-    end
+    create_table :genres do |t|
+      t.string :name
   end
 end
