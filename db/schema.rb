a71316368beb00c80109a5f38318f38a9cf3c674
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-09 12:38:52 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
